b4ef90e09001db364d7d324f5c2690c13c999827
adamkoziol <adam.koziol@inspection.gc.ca>
2023-06-14 18:03:25 +0000

    Create and populate setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+__author__ = 'adamkoziol'
+__author__ = 'LargeKowalski888'
+__author__ = 'noorshu'
+
+setup(
+    name="MiseqHashR",
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    author="Adam Koziol",
+    author_email="adam.koziol@inspection.gc.ca",
+    url="https://github.com/OLC-LOC-Bioinformatics/MiSeqHashR",
+)
